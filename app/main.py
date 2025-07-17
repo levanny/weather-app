edf2393001003fa2d9f73e801617f9e1356b4bda
levanny <lomidzelevani21@gmail.com>
2025-07-17 13:12:52 +0400

    added `models.py` and `database.py` for database initialization and settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.weather import get_weather
+from app.weather import get_weather_data
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/weather")
 async def read_weather(city: str):
-    weather = await get_weather(city)
+    weather = await get_weather_data(city)
     return weather
--- app/weather.py
+++ app/weather.py
@@ -1,19 +1,27 @@
 import httpx
 import os
+from typing import Optional
 
 API_KEY = os.getenv("WEATHER_API_KEY")
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-async def get_weather(city: str):
+async def get_weather_data(city: str):
+    if not API_KEY:
+        raise EnvironmentError("Weather API_KEY not found in the environment variables")
     params = {"q": city, "appid": API_KEY, "units": "metric"}
+
     async with httpx.AsyncClient() as client:
-        response = await client.get(BASE_URL, params=params)
-        if response.status_code == 200:
+
+        try:
+            response = await client.get(BASE_URL, params=params)
+            response.raise_for_status()
             data = response.json()
             return {
-                "city": data["name"],
-                "temperature": data["main"]["temp"],
-                "description": data["weather"][0]["description"]
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"]
             }
-        else:
-            return {"error": "City not found or API error."}
+        except httpx.HTTPStatusError as e:
+            return{"error": f"API returned {e.response.status_code}"}
+        except  Exception as e:
+            return {"error": f"Unexpected error: {str(e)}"}
