0465a549478a87d7035b52bcf216ad85924530d4
levanny <lomidzelevani21@gmail.com>
2025-08-18 16:31:29 +0400

    added cities list to fetch & keep data for. added variable interval

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
 from pydantic_settings import BaseSettings
+from typing import List
 
 class Settings(BaseSettings):
-    CITIES = ["kutaisi", "batumi", "samtredia", "tbilisi", "zugdidi"]
+    cities: List[str] = ["kutaisi", "batumi", "samtredia", "tbilisi", "zugdidi"]
     api_key: str
     base_url: str = "https://api.openweathermap.org/data/2.5/weather"
     database_url: str
+    interval: int = 60
 
     class Config:
         env_file = ".env"
