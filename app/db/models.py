0bbf0a3060590f6589311a588fa1f569141679f9
levanny <lomidzelevani21@gmail.com>
2025-08-29 21:21:07 +0400

    added use class in models

--- app/db/models.py
+++ app/db/models.py
@@ -15,3 +15,12 @@ class Weather(Base):
     description = Column(String)
     icon = Column(String)
     timestamp = Column(DateTime, default=datetime.utcnow)
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
